14197b4ca75aafb8cb93c49c5866d00eb88eebf9
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-09-21 12:48:18 +0300

    update

--- alexa-gadgets-mindstorms/agt-missions/mission-03-orginal/mission-03.py
+++ alexa-gadgets-mindstorms/agt-missions/mission-03-orginal/mission-03.py
@@ -34,23 +34,24 @@ class Direction(Enum):
     The list of directional commands and their variations.
     These variations correspond to the skill slot values.
     """
-    OPEN = ['open', 'fully open', 'sun', 'sunny', 'fully', 'half']
+    FORWARD = ['forward', 'forwards', 'go forward']
+    BACKWARD = ['back', 'backward', 'backwards', 'go backward']
+    LEFT = ['left', 'go left']
+    RIGHT = ['right', 'go right']
+    STOP = ['stop', 'brake']
 
 
-class OpenCurtains(Enum):
+class Command(Enum):
     """
-    The list of directional commands and their variations.
+    The list of preset commands and their invocation variation.
     These variations correspond to the skill slot values.
     """
-    OPEN = ['open', 'fully open', 'sun', 'sunny']
+    MOVE_CIRCLE = ['circle', 'spin']
+    MOVE_SQUARE = ['square']
+    PATROL = ['patrol', 'guard mode', 'sentry mode']
+    FIRE_ONE = ['cannon', '1 shot', 'one shot']
+    FIRE_ALL = ['all shot']
 
-
-class Speed(Enum):
-    """
-    The list of directional commands and their variations.
-    These variations correspond to the skill slot values.
-    """
-    SPEED = ['fast', 'slow']
 
 class MindstormsGadget(AlexaGadget):
     """
@@ -64,10 +65,17 @@ class MindstormsGadget(AlexaGadget):
         """
         super().__init__()
 
+        # Gadget state
+        self.patrol_mode = False
+
         # Ev3dev initialization
         self.leds = Leds()
         self.sound = Sound()
-        self.drive = MoveTank(OUTPUT_A, OUTPUT_B)
+        self.drive = MoveTank(OUTPUT_B, OUTPUT_A)
+        self.weapon = MediumMotor(OUTPUT_A)
+
+        # Start threads
+        threading.Thread(target=self._patrol_thread, daemon=True).start()
 
     def on_connected(self, device_addr):
         """
@@ -92,7 +100,6 @@ class MindstormsGadget(AlexaGadget):
         Handles the Custom.Mindstorms.Gadget control directive.
         :param directive: the custom directive with the matching namespace and name
         """
-        print("Yo!Yo!")
         try:
             payload = json.loads(directive.payload.decode("utf-8"))
             print("Control payload: {}".format(payload), file=sys.stderr)
@@ -100,7 +107,7 @@ class MindstormsGadget(AlexaGadget):
             if control_type == "move":
 
                 # Expected params: [direction, duration, speed]
-                self._move(payload["speed"])
+                self._move(payload["direction"], int(payload["duration"]), int(payload["speed"]))
 
             if control_type == "command":
                 # Expected params: [command]
@@ -109,7 +116,7 @@ class MindstormsGadget(AlexaGadget):
         except KeyError:
             print("Missing expected parameters: {}".format(directive), file=sys.stderr)
 
-    def _move(self, direction, is_blocking=False):
+    def _move(self, direction, duration: int, speed: int, is_blocking=False):
         """
         Handles move commands from the directive.
         Right and left movement can under or over turn depending on the surface type.
@@ -118,12 +125,72 @@ class MindstormsGadget(AlexaGadget):
         :param speed: the speed percentage as an integer
         :param is_blocking: if set, motor run until duration expired before accepting another command
         """
-        print("Move command: ({}, {}, {}, {})".format(direction, speed, is_blocking), file=sys.stderr)
-        if direction in Direction.OPEN.value:
-            self.drive.on_for_seconds(SpeedPercent(200), SpeedPercent(200), 5, block=is_blocking)
+        print("Move command: ({}, {}, {}, {})".format(direction, speed, duration, is_blocking), file=sys.stderr)
+        if direction in Direction.FORWARD.value:
+            self.drive.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed), duration, block=is_blocking)
 
-        if speed in Speed.SPEED.value:
-            print("test!")
+        if direction in Direction.BACKWARD.value:
+            self.drive.on_for_seconds(SpeedPercent(-speed), SpeedPercent(-speed), duration, block=is_blocking)
+
+        if direction in (Direction.RIGHT.value + Direction.LEFT.value):
+            self._turn(direction, speed)
+            self.drive.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed), duration, block=is_blocking)
+
+        if direction in Direction.STOP.value:
+            self.drive.off()
+            self.patrol_mode = False
+
+    def _activate(self, command, speed=50):
+        """
+        Handles preset commands.
+        :param command: the preset command
+        :param speed: the speed if applicable
+        """
+        print("Activate command: ({}, {})".format(command, speed), file=sys.stderr)
+        if command in Command.MOVE_CIRCLE.value:
+            self.drive.on_for_seconds(SpeedPercent(int(speed)), SpeedPercent(5), 12)
+
+        if command in Command.MOVE_SQUARE.value:
+            for i in range(4):
+                self._move("right", 2, speed, is_blocking=True)
+
+        if command in Command.PATROL.value:
+            # Set patrol mode to resume patrol thread processing
+            self.patrol_mode = True
+
+
+    def _turn(self, direction, speed):
+        """
+        Turns based on the specified direction and speed.
+        Calibrated for hard smooth surface.
+        :param direction: the turn direction
+        :param speed: the turn speed
+        """
+        if direction in Direction.LEFT.value:
+            self.drive.on_for_seconds(SpeedPercent(0), SpeedPercent(speed), 2)
+
+        if direction in Direction.RIGHT.value:
+            self.drive.on_for_seconds(SpeedPercent(speed), SpeedPercent(0), 2)
+
+    def _patrol_thread(self):
+        """
+        Performs random movement when patrol mode is activated.
+        """
+        while True:
+            while self.patrol_mode:
+                print("Patrol mode activated randomly picks a path", file=sys.stderr)
+                direction = random.choice(list(Direction))
+                duration = random.randint(1, 5)
+                speed = random.randint(1, 4) * 25
+
+                while direction == Direction.STOP:
+                    direction = random.choice(list(Direction))
+
+                # direction: all except stop, duration: 1-5s, speed: 25, 50, 75, 100
+                self._move(direction.value[0], duration, speed)
+                time.sleep(duration)
+            time.sleep(1)
+
 
 if __name__ == '__main__':
 
